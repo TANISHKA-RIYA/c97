f53bf5f486f0e4784e50a5df65212501299dc413
TANISHKA-RIYA <106598143+TANISHKA-RIYA@users.noreply.github.com>
2023-04-12 21:15:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function toaddroom(){
+    window.location="addition.html";
+}
+function tosubroom(){
+    window.location="subtraction.html";
+}
+function tomulroom(){
+    window.location="multiplication.html";
+}
+function todivroom(){
+    window.location="division.html";
+}
